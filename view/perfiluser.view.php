12cb0903813de922f79fed8990ab074cc1e90849
jeferMLG <jeferbercasa04@gmail.com>
2025-09-18 00:23:36 -0500

    error de perfil

--- view/perfiluser.view.php
+++ view/perfiluser.view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Perfil - Aulapp</title>
-    <link rel="stylesheet" href="../view/css/perfiluser.css">
+    <link rel="stylesheet" href="/ActualizacionInventario/view/Css/perfiluser.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
