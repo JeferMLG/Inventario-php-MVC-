5e3a91174e294d0a044ae8cb23a2e8298e2ceb53
jeferMLG <jeferbercasa04@gmail.com>
2025-09-18 16:41:04 -0500

    ...

--- view/login.view.php
+++ view/login.view.php
@@ -6,7 +6,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Tu CSS personalizado -->
-    <link rel="stylesheet" href="../view/Css/login.css">
+    <link rel="stylesheet" href="view/Css/login.css">
     <!-- Favicon (opcional, para evitar warnings) -->
     <link rel="icon" href="data:,">
 </head>
@@ -14,8 +14,8 @@
 
     <div class="card shadow-sm p-4" style="max-width: 400px; width: 100%;">
         <div class="card-body">
-            <h2 class="text-center mb-4">Accesoo</h2>
-            
+            <h2 class="text-center mb-4">Acceso</h2>
+
             <form action="index.php?vista=login&accion=validar" method="POST" class="needs-validation" novalidate>
                 <!-- Usuario -->
                 <div class="mb-3">
