48587384853b1f9274387a943db3505568dbd2a0
jeferMLG <jeferbercasa04@gmail.com>
2025-09-18 16:39:39 -0500

    .

--- view/login.view.php
+++ view/login.view.php
@@ -14,7 +14,7 @@
 
     <div class="card shadow-sm p-4" style="max-width: 400px; width: 100%;">
         <div class="card-body">
-            <h2 class="text-center mb-4">Acceso</h2>
+            <h2 class="text-center mb-4">Accesoo</h2>
             
             <form action="index.php?vista=login&accion=validar" method="POST" class="needs-validation" novalidate>
                 <!-- Usuario -->
