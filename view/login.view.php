0329fa5ca7a5c54eadd5f9969fce69d4afed8516
jeferMLG <jeferbercasa04@gmail.com>
2025-09-19 14:17:31 -0500

    Arreglo css de login

--- view/login.view.php
+++ view/login.view.php
@@ -6,6 +6,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Tu CSS personalizado -->
+    <link rel="stylesheet" href="../view/Css/login1.css">
     <!-- Favicon (opcional, para evitar warnings) -->
     <link rel="icon" href="data:,">
 </head>
@@ -14,7 +15,7 @@
     <div class="card shadow-sm p-4" style="max-width: 400px; width: 100%;">
         <div class="card-body">
             <h2 class="text-center mb-4">Acceso</h2>
-
+            
             <form action="index.php?vista=login&accion=validar" method="POST" class="needs-validation" novalidate>
                 <!-- Usuario -->
                 <div class="mb-3">
@@ -38,7 +39,7 @@
 
             <!-- Registro -->
             <p class="mt-3 text-center">
-                ¿No tienes una cuenta.? 
+                ¿No tienes una cuenta? 
                 <a href="index.php?vista=register" class="text-decoration-none">Regístrate aquí</a>
             </p>
         </div>
